bfb58d809a0c85e8c3640ccbba4b6ea63a0b7be0
RED2VIOLET <46874094+RED2VIOLET@users.noreply.github.com>
2019-02-08 09:28:50 -0800

    Add files via upload
    add first lesson

---
+++ PhotonConnect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonConnect : MonoBehaviour {
+
+    public string versionName = "0.1";
+    public GameObject sectionView1, sectionView2, sectionView3;
+
+
+    public void connectToPhoton() {
+ 
+        PhotonNetwork.ConnectUsingSettings (versionName);
+
+        Debug.Log ("Connecting to photon server...");
+
+    }
+    private void OnConnectedToMaster() {
+        PhotonNetwork.JoinLobby  (TypedLobby.Default);
+
+        Debug.Log ("We are connected to the Master");
+
+    }
+    private void OnJoinedLobby() {
+        sectionView1.SetActive (true);
+        sectionView2.SetActive (true);
+
+        Debug.Log ("On Joined Lobby");
+    }
+   
+   
+
+    private void OnDisconnectedFromPhoton(){
+
+        if (sectionView1.active)
+            sectionView1.SetActive(false);
+        if (sectionView2.active)
+            sectionView2.SetActive(false);
+
+        sectionView3.SetActive(true);
+
+        Debug.Log ("Disconnect from Photon services");
+
+    }
+    
+    
+}
